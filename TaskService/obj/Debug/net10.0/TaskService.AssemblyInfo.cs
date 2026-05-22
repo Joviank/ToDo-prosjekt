798739b7637ecf6ddbd7f9d7e54a631876736696
Joviank <Joviak@hotmail.no>
2026-05-22 12:40:39 +0200

    Testing av server

--- TaskService/obj/Debug/net10.0/TaskService.AssemblyInfo.cs
+++ TaskService/obj/Debug/net10.0/TaskService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5acf14f646a5e1297df5da033bd13fea773a5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ef07478a91fd2eda816917ea78457c88a6b39b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
